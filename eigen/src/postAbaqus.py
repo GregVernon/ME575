d37fb67c1937097a2f04230582282ae3511915cd
Greg Vernon <greg.j.vernon@gmail.com>
2020-01-06 18:20:06 -0700

    Update postAbaqus.py

--- eigen/src/postAbaqus.py
+++ eigen/src/postAbaqus.py
@@ -1,9 +1,20 @@
+import os
+import sys
+
 def main():
+	f = evaluateObjectiveFunction()
+	writeObjectiveValue(f)
+	
+def evaluateObjectiveFunction():
 	session.openOdb('C:/Users/gregj/Documents/Abaqus/Temp/drumEigen.odb')
 	odb = session.odbs['C:/Users/gregj/Documents/Abaqus/Temp/drumEigen.odb']
 	f = odb.steps['Step-1'].frames[1].frequency
 	odb.close()
 	return f
+	
+def writeObjectiveValue(f):
+	fRes = open("C:/Users/gregj/Documents/Abaqus/Temp/objectiveFunction.csv","w+")
+	fRes.write(str(f))
 
-if __name__ == "__main__"
+if __name__ == "__main__":
 	f = main()
