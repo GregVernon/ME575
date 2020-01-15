3600689798d32963a4b85d6af27c534a0cff919f
Greg Vernon <greg.j.vernon@gmail.com>
2020-01-15 16:38:53 -0700

    Split Belegundu and Ning methods into separate cells

--- homework/HW_1/brachiostone/brachiostoneOptimization.m
+++ homework/HW_1/brachiostone/brachiostoneOptimization.m
@@ -1,7 +1,6 @@
 clear
-close all
 
-nx = 64;
+nx = 16;
 xMin = 0;
 xMax = 1;
 yMin = 0;
@@ -24,19 +23,38 @@ plot(x,y,'--o')
 % Solve via Bernoulli Method
 options = optimset;
 options.MaxFunEvals = 1e6;
+options.MaxIter = 1e4;
 % options.PlotFcns = ["optimplotx","optimplotfval","optimplotfunccount"];
 
 y = fminunc(@(y)belgundu(y,dx,m,g,mu),y,options);
 
-plot(x,y,'-o')
+
+%% Ning Method
+clear
+
+nx = 256;
+xMin = 0;
+xMax = 1;
+yMin = 0;
+yMax = 1;
+
+x = linspace(xMin,xMax,nx);
+dx = x(2) - x(1);
+y = interp1([0 1],[1 0],x);
+y(1) = yMax;
+y(end) = yMin;
+
+mu = 0.0;
+g = 9.81;
+m = 1;
 
 % Solve via Ning method
 H = yMax - yMin;
 options = optimset;
 options.MaxFunEvals = 1e6;
+options.MaxIter = 1e4;
 % options.PlotFcns = ["optimplotx","optimplotfval","optimplotfunccount"];
 y = fminunc(@(y)ning(y,x,H,mu),y,options);
-plot(x,y,'-o')
 
 %% Bezier Geometry
 clear
