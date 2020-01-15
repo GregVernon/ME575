d853bcb448858db0a1ac6e24f102c91975c34093
Greg Vernon <greg.j.vernon@gmail.com>
2020-01-15 16:34:55 -0700

    Add friction to exact solution
    http://mathworld.wolfram.com/BrachistochroneProblem.html

--- homework/HW_1/brachiostone/exactSolution.m
+++ homework/HW_1/brachiostone/exactSolution.m
@@ -1,6 +1,6 @@
-function [x,y] = exactSolution(nPts)
+function [x,y] = exactSolution(fCoeff,nPts)
 a = 0.572917;
 theta = linspace(0,2.412,nPts);
-x = a*(theta - sin(theta));
-y = 1-a*(1-cos(theta));
+x = a*(theta - sin(theta) + fCoeff*(1-cos(theta)));
+y = 1-a*(1-cos(theta) + fCoeff*(theta + sin(theta)));
 end
