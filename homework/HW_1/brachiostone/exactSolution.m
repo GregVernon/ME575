8a3f75663880a70ea6912878c64b4e20318dfda5
Greg Vernon <greg.j.vernon@gmail.com>
2020-01-15 21:13:26 -0700

    Update exactSolution.m

--- homework/HW_1/brachiostone/exactSolution.m
+++ homework/HW_1/brachiostone/exactSolution.m
@@ -1,6 +1,21 @@
-function [x,y] = exactSolution(fCoeff,nPts)
-a = 0.572917;
-theta = linspace(0,2.412,nPts);
-x = a*(theta - sin(theta) + fCoeff*(1-cos(theta)));
-y = 1-a*(1-cos(theta) + fCoeff*(theta + sin(theta)));
+function [x,y] = exactSolution(x,fCoeff,xType)
+if xType == "x-locations"
+    a = 0.572917;
+    % Solve for theta
+    options = optimset;
+    options.MaxFunEvals = 1e6;
+    options.MaxIter = 1e4;
+    theta = reshape(linspace(0,2.412,length(x)),size(x));
+    theta = fminunc(@(theta)thetaObjFun(theta,a,fCoeff,x),theta,options);
+    y = 1-a*(1-cos(theta) + fCoeff*(theta + sin(theta)));
+elseif xType == "#Pts"
+    a = 0.572917;
+    theta = linspace(0,2.412,nPts);
+    x = a*(theta - sin(theta) + fCoeff*(1-cos(theta)));
+    y = 1-a*(1-cos(theta) + fCoeff*(theta + sin(theta)));
+end
+end
+
+function objVal = thetaObjFun(theta,a,fCoeff,x)
+    objVal = sum((a*(theta - sin(theta) + fCoeff*(1-cos(theta))) - x).^2);
 end
