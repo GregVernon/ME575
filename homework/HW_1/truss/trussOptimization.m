faed732d59f1a74f26c04ef3097fd6a80be785c7
Greg Vernon <greg.j.vernon@gmail.com>
2020-01-22 17:18:46 -0700

    Update trussOptimization.m

--- homework/HW_1/truss/trussOptimization.m
+++ homework/HW_1/truss/trussOptimization.m
@@ -2,10 +2,13 @@ A = ones(10,1);
 minArea = 0.1*ones(size(A));
 [mass,stress] = truss(A);
 
-options = optimset;
-% options.Display = "iter-detailed";
-options.TolFun = 1e-9;
-% options.PlotFcns = ["optimplotx","optimplotfval","optimplotfunccount"];
+options = optimoptions('fmincon');
+options.Display = "iter-detailed";
+options.MaxFunctionEvaluations = 1e6;
+options.MaxIterations = 1e6;
+options.OptimalityTolerance = 1e-12;
+options.ConstraintTolerance = 1e-12;
+% options.PlotFcns = {"optimplotx","optimplotfval","optimplotfunccount"};
 
 [A,fval] = fmincon(@truss,A,[],[],[],[],minArea,[],@stressConstraint,options);
 
