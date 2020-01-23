fa896acefd7f41e5097b3277a402048f4d2ec12f
Greg Vernon <greg.j.vernon@gmail.com>
2020-01-22 17:19:50 -0700

    Add high-friction routines

--- homework/HW_1/brachiostone/brachiostoneOptimization.m
+++ homework/HW_1/brachiostone/brachiostoneOptimization.m
@@ -311,4 +311,113 @@ for iter = 1:length(nx)
     % options.PlotFcns = ["optimplotx","optimplotfval","optimplotfunccount"];
     y = fminunc(@(y)ning(y,x,H,mu),y,options);
 %     plot(x,y,'-')
+end%% Bezier Geometry + High Friction
+clear
+close all
+
+xMin = 0;
+xMax = 1;
+yMin = 0;
+yMax = 1;
+domain = [xMin yMax xMax yMin];
+
+bezDegree = 3;
+bezDomain = [0 1];
+nPts = 256;
+bezVariate = linspace(bezDomain(1), bezDomain(2),nPts);
+B = bernsteinPolynomial(bezDegree,bezDomain,bezVariate);
+nDOFS = (bezDegree+1) - 2;
+
+x0 = linspace(xMin,xMax,nDOFS+2);
+x0(1) = [];
+x0(end) = [];
+y0 = interp1([0 1],[1 0],x0);
+
+dofNodes = [x0' y0'];
+dofNodes = reshape(dofNodes',1,numel(dofNodes));
+
+mu = 0.9;
+
+options = optimoptions('fminunc');
+options.MaxFunEvals = 1e6;
+options.MaxIter = 1e4;
+options.TolFun = 1e-14;
+options.TolX = 1e-14;
+options.OptimalityTolerance = 1e-14;
+options.StepTolerance = 1e-14;
+
+muSteps = unique(linspace(0,mu,9));
+for ii = 1:length(muSteps)
+    dofNodes = fminunc(@(dofNodes)ningBezier(dofNodes,domain,muSteps(ii),B),dofNodes,options);
 end
+
+bezNodes = [domain(1) domain(2) dofNodes domain(3) domain(4)];
+bezNodes = transpose(reshape(bezNodes,2,length(bezNodes)/2));
+f = bezier([bezNodes(:,1) bezNodes(:,2)],B);
+x = f(:,1);
+y = f(:,2);
+
+% Exact Solution
+[xExact, yExact] = exactSolution(x,mu,"x-locations");
+yExact = (yExact - yExact(end)) .* ( (yMax-yMin) ./ (yExact(1) - yExact(end)));
+err = y - yExact;
+
+figure;
+subplot(2,1,1)
+hold on
+plot(xExact,yExact);
+plot(x,y,'-o')
+plot(bezNodes(:,1),bezNodes(:,2),'LineStyle','-','Color','k','Marker','o','MarkerFaceColor','k','MarkerEdgeColor','k')
+
+subplot(2,1,2)
+plot(x,err)
+
+%% Ning Method + High Friction
+clear
+
+nx = 256;
+xMin = 0;
+xMax = 1;
+yMin = 0;
+yMax = 1;
+
+x = linspace(xMin,xMax,nx);
+dx = x(2) - x(1);
+y = interp1([0 1],[1 0],x);
+y(1) = yMax;
+y(end) = yMin;
+
+mu = 0.5;
+g = 9.81;
+m = 1;
+
+% Solve via Ning method
+H = yMax - yMin;
+options = optimoptions('fminunc');
+options.MaxFunEvals = 1e6;
+options.MaxIter = 1e4;
+options.TolFun = 1e-14;
+options.TolX = 1e-14;
+options.OptimalityTolerance = 1e-14;
+options.StepTolerance = 1e-14;
+% options.PlotFcns = ["optimplotx","optimplotfval","optimplotfunccount"];
+muSteps = unique(linspace(0,mu,9));
+for ii = 1:length(muSteps)
+    y = fminunc(@(y)ning(y,x,H,muSteps(ii)),y,options);
+end
+% Exact Solution
+[xExact, yExact] = exactSolution(x,mu,"x-locations");
+err = y - yExact;
+
+figure
+hold on
+plot(xExact,yExact)
+plot(x,y,'-')
+
+figure
+subplot(2,1,1)
+plot(x,err)
+hold on
+subplot(2,1,2)
+plot(x,err)
+
