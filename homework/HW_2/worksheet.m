5201dbb0fe64dfb2a7f6627f0988c96de8b1460f
Greg Vernon <greg.j.vernon@gmail.com>
2020-01-29 17:01:29 -0700

    Fix naming in worksheet

--- homework/HW_2/worksheet.m
+++ homework/HW_2/worksheet.m
@@ -1,9 +1,10 @@
 %% Test Simple Quadratic
+clear
 n = 100;
 fun = @(x) x.^2;
 x0 = rand(n,1)*100;
 fun(x0);
-[x,res] = nl_unconstrainedOptimize(fun,x0)
+[x,res] = conjugateGradient(fun,x0,1e-5,1e4)
 
 
 %%
