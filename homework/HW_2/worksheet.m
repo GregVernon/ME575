37e682ec06c6c10e16e7f4135134ed8c59533282
Greg Vernon <greg.j.vernon@gmail.com>
2020-01-29 17:01:41 -0700

    Add test functions from assignment

--- homework/HW_2/worksheet.m
+++ homework/HW_2/worksheet.m
@@ -7,4 +7,16 @@ fun(x0);
 [x,res] = conjugateGradient(fun,x0,1e-5,1e4)
 
 
-%%
+%% Test Matyas Function
+clear
+fun = @(x) 0.26 * (x(1)^2 + x(2)^2) - 0.48 * prod(x); 
+x0 = [20 -20];
+fun(x0);
+[x,res] = conjugateGradient(fun,x0,1e-5,1e4)
+
+%% Test Rosenbrock Function
+clear
+fun = @(x) (1-x(1))^2 + 100 * (x(2) - x(1)^2)^2;
+x0 = [10,10];
+fun(x0);
+[x,res] = conjugateGradient(fun,x0,1e-9,1e5)
