f843102b4abb3fb87a9f0da09d31962722e88583
Greg Vernon <greg.j.vernon@gmail.com>
2020-02-04 22:26:44 -0700

    Update worksheet.m

--- homework/HW_2/worksheet.m
+++ homework/HW_2/worksheet.m
@@ -2,9 +2,9 @@
 clear
 n = 100;
 fun = @(x) x.^2;
-x0 = rand(n,1)*100;
+x0 = rand(1,n)*100;
 fun(x0);
-[x,res] = conjugateGradient(fun,x0,1e-5,1e4)
+[x,res,funEvals] = conjugateGradient(fun,x0,"nl_tol",1e-5,"max_iter",1e4)
 
 
 %% Test Matyas Function
@@ -12,11 +12,44 @@ clear
 fun = @(x) 0.26 * (x(1)^2 + x(2)^2) - 0.48 * prod(x); 
 x0 = [20 -20];
 fun(x0);
-[x,res] = conjugateGradient(fun,x0,1e-5,1e4)
+[x,res,funEvals] = conjugateGradient(fun,x0)
 
 %% Test Rosenbrock Function
 clear
+clc
 fun = @(x) (1-x(1))^2 + 100 * (x(2) - x(1)^2)^2;
+x = sym('x',[1,2],'real');
+f(x) = (1-x(1))^2 + 100 * (x(2) - x(1)^2)^2;
+g(x) = gradient(f);
+% g = @(x) [2*x(1) - 400*x(1)*(- x(1)^2 + x(2)) - 2; -200*x(1)^2 + 200*x(2)]
 x0 = [10,10];
 fun(x0);
-[x,res] = conjugateGradient(fun,x0,1e-9,1e5)
+[x,res,iter,funEvals] = conjugateGradient(fun,x0,"gradFun",g,"searchDirection","Fletcher-Reeves","nl_tol",1e-9,"max_iter",1e4)
+
+%% Test Simple Quadratic
+clear
+n = 100;
+fun = @(x) x.^2;
+x0 = rand(1,n)*100;
+fun(x0);
+[x,res,funEvals] = BFGS(fun,x0,"nl_tol",1e-5,"max_iter",1e4)
+
+%% Test Matyas Function
+clear
+fun = @(x) 0.26 * (x(1)^2 + x(2)^2) - 0.48 * prod(x); 
+x0 = [20 -20];
+fun(x0);
+[x,res,funEvals] = BFGS(fun,x0)
+
+%% Test Rosenbrock Function
+clear
+clc
+fun = @(x) (1-x(1))^2 + 100 * (x(2) - x(1)^2)^2;
+x = sym('x',[1,2],'real');
+f(x) = (1-x(1))^2 + 100 * (x(2) - x(1)^2)^2;
+g(x) = gradient(f);
+% g = @(x) [2*x(1) - 400*x(1)*(- x(1)^2 + x(2)) - 2; -200*x(1)^2 + 200*x(2)]
+x0 = [4,4];
+fun(x0);
+[x,res,iter,funEvals] = BFGS(fun,x0,"gradFun",g,"nl_tol",1e-9,"max_iter",1e4)
+
