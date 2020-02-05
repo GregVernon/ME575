3d2a01feeb96595b8b06ac75c624f221c3ebdfcd
Greg Vernon <greg.j.vernon@gmail.com>
2020-02-04 22:27:09 -0700

    Create runTreilis.py

---
+++ eigen/src/runTreilis.py
@@ -0,0 +1,13 @@
+#!python
+import os
+import sys
+# import numpy
+
+pathToTrelis = "C:/Program Files/Trelis 17.1/bin"
+sys.path.append(pathToTrelis)
+
+# pathToCubit = r"C:\Program Files\Cubit 15.5\bin"
+# sys.path.append(pathToCubit)
+
+import cubit
+cubit.init([''])
